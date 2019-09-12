33efc0ff7d19969324f9c56f54d96f190d587c68
Conrad Dsa <dsa.conrad@gmail.com>
2019-09-12 14:13:11 +0300

    Uploaded Weather Checker

---
+++ weather-checker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# weather-checker.sh
+# Copyright 2019 Conrad. All rights reserved.
+
+program=Weather
+version=1.1
+year=2018
+developer=computer-geek64
+
+case $1 in
+-h | --help)
+	echo "$program $version"
+	echo "Copyright $year $developer. All rights reserved."
+	echo
+	echo "Usage: weather [options]"
+	echo "Option          Long Option             Description"
+	echo "-h              --help                  Show the help screen"
+	echo "-l [location]   --location [location]   Specifies the location"
+	;;
+-l | --location)
+	curl wttr.in/$2
+	;;
+*)
+	curl wttr.in
+	;;
+esac
